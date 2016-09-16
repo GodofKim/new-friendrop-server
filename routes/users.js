b504affa8ecd75b79a8dfa941b96a289600a5bc6
GodofKim <kjh.original@gmail.com>
2016-09-16 14:46:35 +0900

    ADD : users router

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,37 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+/* Authentication */
+const Authentication = require('../controllers/authentication');
+const passportService = require('../services/passport');
+const passport = require('passport');
+const requireAuth = passport.authenticate('jwt', { session: false });
+const requireSignin = passport.authenticate('local', { session: false});
+/* Models */
+const User = require('../models/user');
+const Profile = require('../models/profile');
+const Drop = require('../models/drop');
+const Letter = require('../models/letter');
+const Contact = require('../models/contact');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* APIs */
+router.post('/signup', Authentication.signup);
+
+router.post('/signin', requireSignin, Authentication.signin);
+
+router.delete('/', requireAuth, (req, res, next) => {
+  const email = req.user.email;
+
+  Contact.remove({email})
+  .then(() => Letter.remove({email}), (err) => next(err) )
+  .then(() => Drop.remove({email}), (err) => next(err) )
+  .then(() => Profile.remove({email}), (err) => next(err) )
+  .then(() => User.remove({email}), (err) => next(err))
+  .then(() => {
+    res.status(200);
+    res.send('User info deleted.');
+  }, (err) => {
+    next(err);
+  });
 });
 
 module.exports = router;
