a2eaf1835c170da0b5356fcd0116b4fcf1051d2c
GodofKim <kjh.original@gmail.com>
2016-09-16 16:23:05 +0900

    ADD : profile image upload

--- routes/profile.js
+++ routes/profile.js
@@ -5,6 +5,19 @@ const Profile = require('../models/profile');
 /* Authentication */
 const passport = require('passport');
 const requireAuth = passport.authenticate('jwt', { session: false });
+/* Multer for file upload */
+var multer = require('multer');
+var upload = multer({ dest: 'uploads/' });
+/* AWS SDK */
+var AWS = require('aws-sdk');
+AWS.config.update({
+  signatureVersion: 'v4',
+  accessKeyId: "",
+  secretAccessKey: ""
+});
+const s3 = new AWS.S3();
+const fs = require('fs');
+
 
 /* APIs */
 router.get('/', requireAuth, (req, res, next) => {
@@ -47,5 +60,40 @@ router.get('/:email', (req, res, next) => {
     }
   });
 });
+
+router.post('/image', requireAuth, upload.single('image'), (req, res) => {
+  Profile.findOne({ email: req.user.email }, (err, profile) => {
+    if(err) return next(err);
+    if(!profile){
+      console.log("Find Profile Failed");
+      res.status(404).send("Cannot find profile");
+      return;
+    }
+
+    if(req.file){
+      s3.upload({'Bucket':'friendrop',
+        'Key':req.file.filename.toString(),
+        'ACL':'public-read',
+        'Body': fs.createReadStream(req.file.path),
+        'ContentType':'image/png'},
+        (err, data) => {
+          if(err) throw err;
+          fs.unlink(req.file.path);
+
+          profile.update({$push : {image: req.file.filename}}, (err) => {
+            if(err) next(err);
+            console.log("Image Upload Success");
+
+            res.status(200);
+            res.send("Image Upload Success");
+          });
+      });
+    }else{
+      console.log("file doesn't exist.");
+      res.send("file does't exist.");
+    }
+  });
+});
+
 
 module.exports = router;
