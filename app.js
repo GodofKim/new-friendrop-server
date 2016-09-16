bce8ce893f0f54a5ed267f46e8856fadc013189e
GodofKim <kjh.original@gmail.com>
2016-09-16 13:16:56 +0900

    ADD : connect modules to app

--- app.js
+++ app.js
@@ -1,29 +1,42 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+/* Multer For Upload */
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+/* Routers */
+const routes = require('./routes/index');
+const users = require('./routes/users');
+const profile = require('./routes/profile');
+const drop = require('./routes/drop');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// DB setup
+//mongoose.connect('');
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+/* Routers */
 app.use('/', routes);
 app.use('/users', users);
+app.use('/profile', profile);
+app.use('/drop', drop);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
