05ef4223a9b9b51aa0a398b55e24d4412a65c5fe
GodofKim <kjh.original@gmail.com>
2016-09-24 20:46:13 +0900

    Modify : change server port
    3000은 클라 서버가 쓰기 때문에 api 서버는 4000으로 변경

--- app.js
+++ app.js
@@ -77,7 +77,7 @@ app.use(function(err, req, res, next) {
   });
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log('Friendrop server is running at ', port);
 });
