6bf375ef64ec3d09127fe8cab15d985648b26ac9
GodofKim <kjh.original@gmail.com>
2016-10-01 23:03:55 +0900

    Add : send drops using async foreach
    async.each를 이용해서 드랍 보내기 추가

--- routes/drop.js
+++ routes/drop.js
@@ -1,12 +1,161 @@
 const express = require('express');
 const router = express.Router();
 
+const async = require('async');
+
 const Profile = require('../models/profile');
 const Drop = require('../models/drop');
 const passport = require('passport');
 const requireAuth = passport.authenticate('jwt', { session: false });
 
 /* APIs */
+
+/*
+READ MEMO: GET /api/memo
+*/
+router.get('/', requireAuth, (req, res) => {
+  Drop.find({ host: req.user._id })
+  .sort({"_id": -1})
+  .limit(6)
+  .exec((err, drops) => {
+    if(err) throw err;
+
+    drops = JSON.parse(JSON.stringify(drops));
+    console.log(drops);
+
+    var dropsToSend = [];
+    // 1st para in async.each() is the array of items
+    async.each(drops,
+      // 2nd param is the function that each item is passed to
+      function(drop, callback){
+        // Call an asynchronous function, often a save() to DB
+        Profile.find({email: drop.email}, function (err, profile){
+          if(err) throw err;
+
+          profile = profile[0];
+
+          dropsToSend.push({
+            _id: drop._id, // for sorting, keying
+            email: profile.email,
+            name: profile.name,
+            nickname: profile.nickname,
+            gender: profile.gender,
+            school: profile.school,
+            major: profile.major,
+            date: drop.date
+          });
+
+          // Async call is done, alert via callback
+          callback();
+        });
+      },
+      // 3rd param is the function to call when everything's done
+      function(err){
+        // All tasks are done now
+        console.log(dropsToSend);
+        return res.json(dropsToSend);
+      }
+    );
+  });
+});
+
+/* READ ADDITIONAL OLD/NEW MEMO: GET /api/memo/:listType/:id */
+router.get('/:listType/:id', requireAuth, (req, res) => {
+  let listType = req.params.listType;
+  let id = req.params.id;
+
+  // CHECK LIST TYPE VALIDITY
+  if(listType !== 'old' && listType !== 'new') {
+    return res.status(400).json({
+      error: "INVALID LISTTYPE",
+      code: 1
+    });
+  }
+
+  // CHECK MEMO ID VALIDITY
+  if(!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({
+      error: "INVALID ID",
+      code: 2
+    });
+  }
+
+  let objId = new mongoose.Types.ObjectId(req.params.id);
+
+  if(listType === 'new'){
+    // GET NEWER MEMO
+    var dropsToSend = [];
+    // 1st para in async.each() is the array of items
+    async.each(drops,
+      // 2nd param is the function that each item is passed to
+      function(drop, callback){
+        // Call an asynchronous function, often a save() to DB
+        Profile.find({email: drop.email, _id: { $gt: objId }}, function (err, profile){
+          if(err) throw err;
+
+          profile = profile[0];
+
+          dropsToSend.push({
+            _id: drop._id, // for sorting, keying
+            email: profile.email,
+            name: profile.name,
+            nickname: profile.nickname,
+            gender: profile.gender,
+            school: profile.school,
+            major: profile.major,
+            date: drop.date
+          });
+
+          // Async call is done, alert via callback
+          callback();
+        });
+      },
+      // 3rd param is the function to call when everything's done
+      function(err){
+        // All tasks are done now
+        console.log(dropsToSend);
+        return res.json(dropsToSend);
+      }
+    );
+  } else {
+    // GET OLDER MEMO
+    var dropsToSend = [];
+    // 1st para in async.each() is the array of items
+    async.each(drops,
+      // 2nd param is the function that each item is passed to
+      function(drop, callback){
+        // Call an asynchronous function, often a save() to DB
+        Profile.find({email: drop.email, _id: { $lt: objId }}, function (err, profile){
+          if(err) throw err;
+
+          profile = profile[0];
+
+          dropsToSend.push({
+            _id: drop._id, // for sorting, keying
+            email: profile.email,
+            name: profile.name,
+            nickname: profile.nickname,
+            gender: profile.gender,
+            school: profile.school,
+            major: profile.major,
+            date: drop.date
+          });
+
+          // Async call is done, alert via callback
+          callback();
+        });
+      },
+      // 3rd param is the function to call when everything's done
+      function(err){
+        // All tasks are done now
+        console.log(dropsToSend);
+        return res.json(dropsToSend);
+      }
+    );
+  }
+});
+
+/*
 // 일단 이렇게 하고.. 나중에 더 좋은 방법을 찾아보자.
 router.get('/', requireAuth, (req, res, next) => {
   Drop.find({ host: req.user._id}, (err, drops) => {
@@ -40,6 +189,7 @@ router.get('/', requireAuth, (req, res, next) => {
     });
   });
 });
+*/
 
 router.delete('/', function(req, res, next) {
   res.render('index', {title: 'exrpess'});
