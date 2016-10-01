2c017e4a74c7cd694198a947129d02c207eb4f7d
Godof Kim <kjh.original@gmail.com>
2016-10-02 00:59:51 +0900

    Bug Fix : load until scrollable bug fixed from server
    완전히 논리가 잘못되어있어서 바로 잡음. 고침.

--- routes/drop.js
+++ routes/drop.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const async = require('async');
+const mongoose = require('mongoose');
 
 const Profile = require('../models/profile');
 const Drop = require('../models/drop');
@@ -81,115 +82,82 @@ router.get('/:listType/:id', requireAuth, (req, res) => {
   }
 
   var objId = new mongoose.Types.ObjectId(req.params.id);
+  var option;
 
-  if(listType === 'new'){
-    // GET NEWER MEMO
-    var dropsToSend = [];
-    // 1st para in async.each() is the array of items
-    async.each(drops,
-      // 2nd param is the function that each item is passed to
-      function(drop, callback){
-        // Call an asynchronous function, often a save() to DB
-        Profile.find({email: drop.email, _id: { $gt: objId }}, function (err, profile){
-          if(err) throw err;
+  if(listType === 'new')
+  {
+    Drop.find({ host: req.user._id, _id: { $gt: objId }})
+    .sort({"_id": -1})
+    .limit(6)
+    .exec((err, drops) => {
+      if(err) throw err;
 
-          profile = profile[0];
+      var dropsToSend = [];
+      async.each(
+        drops,
+        (drop, callback) => {
+          Profile.find({email: drop.email}, (err, profile) => {
+            if(err) throw err;
+            profile = profile[0];
 
-          dropsToSend.push({
-            _id: drop._id, // for sorting, keying
-            email: profile.email,
-            name: profile.name,
-            nickname: profile.nickname,
-            gender: profile.gender,
-            school: profile.school,
-            major: profile.major,
-            date: drop.date
+            dropsToSend.push({
+              _id: drop._id, // for sorting, keying
+                email: profile.email,
+                name: profile.name,
+                nickname: profile.nickname,
+                gender: profile.gender,
+                school: profile.school,
+                major: profile.major,
+                date: drop.date
+            });
+            callback();
           });
-
-          // Async call is done, alert via callback
-          callback();
+        },
+        (err) => {
+          // All tasks are done now
+            console.log(dropsToSend);
+            return res.json(dropsToSend);
         });
-      },
-      // 3rd param is the function to call when everything's done
-      function(err){
-        // All tasks are done now
-        console.log(dropsToSend);
-        return res.json(dropsToSend);
-      }
-    );
-  } else {
-    // GET OLDER MEMO
-    var dropsToSend = [];
-    // 1st para in async.each() is the array of items
-    async.each(drops,
-      // 2nd param is the function that each item is passed to
-      function(drop, callback){
-        // Call an asynchronous function, often a save() to DB
-        Profile.find({email: drop.email, _id: { $lt: objId }}, function (err, profile){
-          if(err) throw err;
-
-          profile = profile[0];
-
-          dropsToSend.push({
-            _id: drop._id, // for sorting, keying
-            email: profile.email,
-            name: profile.name,
-            nickname: profile.nickname,
-            gender: profile.gender,
-            school: profile.school,
-            major: profile.major,
-            date: drop.date
-          });
-
-          // Async call is done, alert via callback
-          callback();
-        });
-      },
-      // 3rd param is the function to call when everything's done
-      function(err){
-        // All tasks are done now
-        console.log(dropsToSend);
-        return res.json(dropsToSend);
-      }
-    );
-  }
-});
-
-/*
-// 일단 이렇게 하고.. 나중에 더 좋은 방법을 찾아보자.
-router.get('/', requireAuth, (req, res, next) => {
-  Drop.find({ host: req.user._id}, (err, drops) => {
-    if (err) return next(err);
-
-    var fetchData = JSON.parse(JSON.stringify(drops));
-    var sendArray = [];
-    var times = fetchData.length;
-    var i = 0;
-
-    fetchData.forEach((drop) => {
-      Profile.findOne({email: drop.email}, (err, profile) => {
-        var fetchProfile = JSON.parse(JSON.stringify(profile));
-
-        sendArray[i] = {
-          _id: drop._id,
-          email: fetchProfile.email,
-          name: fetchProfile.name,
-          nickname: fetchProfile.nickname,
-          gender: fetchProfile.gender,
-          school: fetchProfile.school,
-          major: fetchProfile.major,
-          date: drop.date
-        };
-
-        i ++;
-        if( i == times ){
-          res.json({drops: sendArray});
-        }
-      });
     });
-  });
-});
-*/
+  }
+  else
+  {
+    Drop.find({ host: req.user._id, _id: { $lt: objId }})
+    .sort({"_id": -1})
+    .limit(6)
+    .exec((err, drops) => {
+      if(err) throw err;
+
+      var dropsToSend = [];
+      async.each(
+        drops,
+        (drop, callback) => {
+          Profile.find({email: drop.email}, (err, profile) => {
+            if(err) throw err;
+            profile = profile[0];
+
+            dropsToSend.push({
+              _id: drop._id, // for sorting, keying
+                email: profile.email,
+                name: profile.name,
+                nickname: profile.nickname,
+                gender: profile.gender,
+                school: profile.school,
+                major: profile.major,
+                date: drop.date
+            });
+            callback();
+          });
+        },
+        (err) => {
+          // All tasks are done now
+            console.log(dropsToSend);
+            return res.json(dropsToSend);
+        });
+    });
+  }
+}); // router.get('/:listType/:id', requireAuth, (req, res)=>{} );
+
 
 router.delete('/', function(req, res, next) {
   res.render('index', {title: 'exrpess'});
