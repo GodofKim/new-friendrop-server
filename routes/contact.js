f95b543bb4b1589963af356d27f1bf0b39e63a24
Godof Kim <kjh.original@gmail.com>
2016-10-02 01:35:23 +0900

    Modify : change how to fetch data ( sync=> async)
    훨씬 좋은 방법으로 바꾸다

--- routes/contact.js
+++ routes/contact.js
@@ -1,43 +1,154 @@
 const express = require('express');
 const router = express.Router();
 
+const async = require('async');
+const mongoose = require('mongoose');
+
 const User = require('../models/user');
 const Profile = require('../models/profile');
 const Contact = require('../models/contact');
 const passport = require('passport');
 const requireAuth = passport.authenticate('jwt', { session: false });
 
-
-router.get('/', requireAuth, (req, res, next) => {
-  Contact.find({host: req.user._id}, (err, contacts) => {
+router.get('/', requireAuth, (req, res) => {
+  Contact.find({ host: req.user._id })
+  .sort({"_id": -1})
+  .limit(6)
+  .exec((err, contacts) => {
     if(err) throw err;
 
-    var fetchData = JSON.parse(JSON.stringify(contacts));
-    var sendArray = [];
-    var times = fetchData.length;
-    var i = 0;
+    contacts = JSON.parse(JSON.stringify(contacts));
+    console.log(contacts);
 
-    fetchData.forEach((contact) => {
-      Profile.findOne({email: contact.email}, (err, profile) => {
-        var fetchProfile = JSON.parse(JSON.stringify(profile));
+    var contactsToSend = [];
+    // 1st para in async.each() is the array of items
+    async.each(contacts,
+      // 2nd param is the function that each item is passed to
+      function(contact, callback){
+        // Call an asynchronous function, often a save() to DB
+        Profile.find({email: contact.email}, function (err, profile){
+          if(err) throw err;
 
-        sendArray[i] = {
-          _id: contact._id,
-          email: fetchProfile.email,
-          name: fetchProfile.name,
-          nickname: fetchProfile.nickname,
-          phone: fetchProfile.phone,
-          date: contact.date
-        };
+          profile = profile[0];
 
-        i ++;
-        if( i == times ){
-          res.json({contacts: sendArray});
-        }
-      });
-    });
+          contactsToSend.push({
+            _id: contact._id,
+            email: profile.email,
+            name: profile.name,
+            nickname: profile.nickname,
+            phone: profile.phone,
+            date: contact.date
+          });
+
+          // Async call is done, alert via callback
+          callback();
+        });
+      },
+      // 3rd param is the function to call when everything's done
+      function(err){
+        // All tasks are done now
+        console.log(contactsToSend);
+        return res.json(contactsToSend);
+      }
+    );
   });
 });
+
+/* READ ADDITIONAL OLD/NEW MEMO: GET /api/memo/:listType/:id */
+router.get('/:listType/:id', requireAuth, (req, res) => {
+  var listType = req.params.listType;
+  var id = req.params.id;
+
+  // CHECK LIST TYPE VALIDITY
+  if(listType !== 'old' && listType !== 'new') {
+    return res.status(400).json({
+      error: "INVALID LISTTYPE",
+      code: 1
+    });
+  }
+
+  // CHECK MEMO ID VALIDITY
+  if(!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({
+      error: "INVALID ID",
+      code: 2
+    });
+  }
+
+  var objId = new mongoose.Types.ObjectId(req.params.id);
+
+  if(listType === 'new')
+  {
+    Contact.find({ host: req.user._id, _id: { $gt: objId }})
+    .sort({"_id": -1})
+    .limit(6)
+    .exec((err, contacts) => {
+      if(err) throw err;
+
+      var contactsToSend = [];
+      async.each(
+        contacts,
+        (contact, callback) => {
+          Profile.find({email: contact.email}, (err, profile) => {
+            if(err) throw err;
+            profile = profile[0];
+
+            contactsToSend.push({
+              _id: contact._id,
+              email: profile.email,
+              name: profile.name,
+              nickname: profile.nickname,
+              phone: profile.phone,
+              date: contact.date
+            });
+            callback();
+
+          });
+        },
+        (err) => {
+          // All tasks are done now
+            console.log(contactsToSend);
+            return res.json(contactsToSend);
+        });
+    });
+  }
+  else
+  {
+    Contact.find({ host: req.user._id, _id: { $lt: objId }})
+    .sort({"_id": -1})
+    .limit(6)
+    .exec((err, contacts) => {
+      if(err) throw err;
+
+      var contactsToSend = [];
+      async.each(
+        contacts,
+        (contact, callback) => {
+          Profile.find({email: contact.email}, (err, profile) => {
+            if(err) throw err;
+            profile = profile[0];
+
+            contactsToSend.push({
+              _id: contact._id,
+              email: profile.email,
+              name: profile.name,
+              nickname: profile.nickname,
+              phone: profile.phone,
+              date: contact.date
+            });
+            callback();
+
+          });
+        },
+        (err) => {
+          // All tasks are done now
+            console.log(contactsToSend);
+            return res.json(contactsToSend);
+        });
+    });
+  }
+}); // router.get('/:listType/:id', requireAuth, (req, res)=>{} );
+
 
 router.post ('/', requireAuth, function (req, res, next) {
   const receiver = req.body.receiver;
--- routes/letter.js
+++ routes/letter.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const async = require('async');
+const mongoose = require('mongoose');
 
 const User = require('../models/user');
 const Profile = require('../models/profile');
@@ -8,37 +10,145 @@ const Letter = require('../models/letter');
 const passport = require('passport');
 const requireAuth = passport.authenticate('jwt', { session: false });
 
+router.get('/', requireAuth, (req, res) => {
+  Letter.find({ host: req.user._id })
+  .sort({"_id": -1})
+  .limit(6)
+  .exec((err, letters) => {
+    if(err) throw err;
 
-router.get('/', requireAuth, (req, res, next) => {
-  Letter.find({host: req.user._id}, (err, letters) => {
-    if(err) return next(err);
+    drops = JSON.parse(JSON.stringify(letters));
+    console.log(letters);
 
-    var fetchData = JSON.parse(JSON.stringify(letters));
-    var sendArray = [];
-    var times = fetchData.length;
-    var i = 0;
+    var lettersToSend = [];
+    // 1st para in async.each() is the array of items
+    async.each(letters,
+      // 2nd param is the function that each item is passed to
+      function(letter, callback){
+        // Call an asynchronous function, often a save() to DB
+        Profile.find({email: letter.email}, function (err, profile){
+          if(err) throw err;
 
-    fetchData.forEach((letter) => {
-      Profile.findOne({email: letter.email}, (err, profile) => {
-        var fetchProfile = JSON.parse(JSON.stringify(profile));
+          profile = profile[0];
 
-        sendArray[i] = {
-          _id: letter._id,
-          email: fetchProfile.email,
-          name: fetchProfile.name,
-          nickname: fetchProfile.nickname,
-          content: letter.content,
-          date: letter.date
-        };
+          lettersToSend.push({
+            _id: letter._id,
+            email: profile.email,
+            name: profile.name,
+            nickname: profile.nickname,
+            content: letter.content,
+            date: letter.date
+          });
 
-        i ++;
-        if( i == times ){
-          res.json({letters: sendArray}); // ---- 이 방법이 맘에들지 않는다면 요청/응답을 두번에 걸쳐서 하는 방법도 있다.
-        }
-      });
-    });
+          // Async call is done, alert via callback
+          callback();
+        });
+      },
+      // 3rd param is the function to call when everything's done
+      function(err){
+        // All tasks are done now
+        console.log(lettersToSend);
+        return res.json(lettersToSend);
+      }
+    );
   });
 });
+
+/* READ ADDITIONAL OLD/NEW MEMO: GET /api/memo/:listType/:id */
+router.get('/:listType/:id', requireAuth, (req, res) => {
+  var listType = req.params.listType;
+  var id = req.params.id;
+
+  // CHECK LIST TYPE VALIDITY
+  if(listType !== 'old' && listType !== 'new') {
+    return res.status(400).json({
+      error: "INVALID LISTTYPE",
+      code: 1
+    });
+  }
+
+  // CHECK MEMO ID VALIDITY
+  if(!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({
+      error: "INVALID ID",
+      code: 2
+    });
+  }
+
+  var objId = new mongoose.Types.ObjectId(req.params.id);
+
+  if(listType === 'new')
+  {
+    Letter.find({ host: req.user._id, _id: { $gt: objId }})
+    .sort({"_id": -1})
+    .limit(6)
+    .exec((err, letters) => {
+      if(err) throw err;
+
+      var lettersToSend = [];
+      async.each(
+        letters,
+        (letter, callback) => {
+          Profile.find({email: letter.email}, (err, profile) => {
+            if(err) throw err;
+            profile = profile[0];
+
+            lettersToSend.push({
+              _id: letter._id,
+              email: profile.email,
+              name: profile.name,
+              nickname: profile.nickname,
+              content: letter.content,
+              date: letter.date
+            });
+            callback();
+
+          });
+        },
+        (err) => {
+          // All tasks are done now
+            console.log(lettersToSend);
+            return res.json(lettersToSend);
+        });
+    });
+  }
+  else
+  {
+    Letter.find({ host: req.user._id, _id: { $lt: objId }})
+    .sort({"_id": -1})
+    .limit(6)
+    .exec((err, letters) => {
+      if(err) throw err;
+
+      var lettersToSend = [];
+      async.each(
+        letters,
+        (letter, callback) => {
+          Profile.find({email: letter.email}, (err, profile) => {
+            if(err) throw err;
+            profile = profile[0];
+
+            lettersToSend.push({
+              _id: letter._id,
+              email: profile.email,
+              name: profile.name,
+              nickname: profile.nickname,
+              content: letter.content,
+              date: letter.date
+            });
+            callback();
+
+          });
+        },
+        (err) => {
+          // All tasks are done now
+            console.log(lettersToSend);
+            return res.json(lettersToSend);
+        });
+    });
+  }
+}); // router.get('/:listType/:id', requireAuth, (req, res)=>{} );
+
 
 router.post ('/', requireAuth, function (req, res, next) {
   const receiver = req.body.receiver;
